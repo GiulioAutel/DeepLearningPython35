5d7ebc5f82f07622bb2c6e8c0fc42445b5374ae1
GiulioAutel <giulioautel@gmail.com>
2019-10-22 11:16:53 +0200

    Code cleanup

--- network.py
+++ network.py
@@ -135,15 +135,11 @@ class Network(object):
         return sum(int(x == y) for (x, y) in test_results)
 
     def cost_derivative(self, output_activations, y):
-        """Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
         return (output_activations-y)
 
 #### Miscellaneous functions
 def sigmoid(z):
-    """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
 
 def sigmoid_prime(z):
-    """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
